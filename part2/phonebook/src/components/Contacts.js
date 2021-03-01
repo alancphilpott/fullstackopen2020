aa624ff3c10baa123f783db775c876058526bb9a
alancphilpott <alanp1995@gmail.com>
2021-03-01 13:34:08 +0000

    function component shorthand form

--- part2/phonebook/src/components/Contacts.js
+++ part2/phonebook/src/components/Contacts.js
@@ -1,14 +1,12 @@
 import React from 'react'
 import Contact from './Contact'
 
-const Contacts = ({ contacts }) => {
-  return (
-    <div className={'contacts'}>
-      {contacts.map((p) => (
-        <Contact key={p.name} person={p} />
-      ))}
-    </div>
-  )
-}
+const Contacts = ({ contacts }) => (
+  <div className={'contacts'}>
+    {contacts.map((p) => (
+      <Contact key={p.id} person={p} />
+    ))}
+  </div>
+)
 
 export default Contacts
--- part2/phonebook/src/components/Filter.js
+++ part2/phonebook/src/components/Filter.js
@@ -1,12 +1,10 @@
 import React from 'react'
 
-const Filter = ({ search, handleClick }) => {
-  return (
-    <div className={'filter'}>
-      <header>Search Contact</header>
-      <input type="text" value={search} onChange={handleClick} />
-    </div>
-  )
-}
+const Filter = ({ search, handleClick }) => (
+  <div className={'filter'}>
+    <header>Search Contact</header>
+    <input type="text" value={search} onChange={handleClick} />
+  </div>
+)
 
 export default Filter
